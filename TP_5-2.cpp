f9d3b2d3d2bed5af59b070a8b411679fd032d7d3
carlos guariglia <carlosguariglia@gmail.com>
2024-06-11 20:33:04 -0300

    ejercio 2 terminado

---
+++ TP_5-2.cpp
@@ -0,0 +1,60 @@
+/*
+2. Escribe un programa que lea del teclado una frase y a continuación
+ visualice las palabras de la frase en columna, seguida cada una del número
+ de letras que la componen.
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void clearConsole();
+
+int main()
+{   
+    clearConsole();
+    string frase;
+    int letras = 0;
+    int indice = 0;
+    cout << "Ingrese una frase" << endl;
+    getline(cin, frase);
+    
+    clearConsole();
+    cout << "-------------------------------------" << endl;
+    cout << "La frase: " << frase << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "Palabra    \tCantidad de Letras" << endl;
+    cout << "-------------------------------------" << endl;
+
+
+    while (indice < frase.length())
+    {
+        if (frase[indice] == ' ')
+        {
+            cout << "\t               " << letras << endl;
+            letras = 0;
+        }
+        else
+        {
+            cout << frase[indice];
+            letras++;
+        }
+        indice++;
+    }
+    cout << "\t               " << letras << endl;
+
+
+
+    return 0;
+}
+
+void clearConsole() {
+    #ifdef __unix__
+        system("clear");
+    #elif __APPLE__
+        system("clear");
+    #elif defined(_WIN32) || defined(WIN64)
+        system("cls");
+    #endif
+}
